90bccd4a64122689fdec0f9d2d0297f4eb3acded
samibentebbiche <samibentebbiche@gmail.com>
2023-09-21 19:54:10 +0200

    Setup basic application and Entry point.

---
+++ HazelClone/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// for use bu Hazel Applications
+
+#include "Hazel/Application.h"
+
+
+// --- Entry Point ----------------------
+
+#include "Hazel/EntryPoint.h"
+//----------------------------------------
